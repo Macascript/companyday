897ad855de489bf1f7b54d02f22aa3fc5c4e6abe
Macascript <jotaele.arrojo@gmail.com>
2022-02-21 23:40:18 +0100

    datetime

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ class Charla(db.Model):
     empresa_id = db.Column(db.Integer,db.ForeignKey("empresa.id"),primary_key = True)
     descripcion = db.Column(db.String(500))
     presencial = db.Column(db.Boolean)
-    fecha = db.Column(db.Datetime)
+    fecha = db.Column(db.DateTime)
     ponente = db.Column(db.String(100))
 
     def __init__(self,empresa_id,descripcion,presencial,fecha,ponente):
