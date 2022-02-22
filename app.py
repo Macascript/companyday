3b5e8e1c693b1e3ac385fdc35032500891e48fc4
Macascript <jotaele.arrojo@gmail.com>
2022-02-22 11:26:25 +0100

    upload file

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #from crypt import methods
+import os
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = "/logos"
 app = Flask(__name__)
@@ -177,21 +179,17 @@ def profile():
         poblacion = request.form["poblacion"]
         codigo_postal = request.form["codigo_postal"]
         web = request.form["web"]
-        logo_url = request.form["logo_url"]
 
         # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if 'file' in request.files:
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit a empty part without filename
+            if file.filename != '':
+                if file and allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    logo_url = UPLOAD_FOLDER+"/"+filename
 
         consentimiento_uso_nombre = request.form["consentimiento_uso_nombre"]
         buscando_candidatos = request.form["buscando_candidatos"]
