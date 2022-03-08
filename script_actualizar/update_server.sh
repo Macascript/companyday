d215c706966242ad09f8b44957064f0cc0f6fa90
adrian <j.adrian.mazuecos@gmail.com>
2022-03-08 22:37:02 +0100

    *update_server.sh

--- script_actualizar/update_server.sh
+++ script_actualizar/update_server.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-
-
-while true
-do
-    command=$( git pull )
-    if [[ ${command} == *"app.py"* ]]; then
-        sudo /etc/init.d/apache2 restart
-    fi
-    sleep 1m
-    echo "Hecho"
-done
+command=$( git pull )
+if [[ ${command} == *"app.py"* ]]; then
+    sudo /etc/init.d/apache2 restart
+fi
+sleep 1m
+echo "Hecho"
