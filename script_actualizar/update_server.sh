04586313b7aa09df41d47012df32aff42b77c0f6
Adrian Mazuecos <j.adrian.mazuecos@gmail.com>
2022-03-08 11:39:25 +0100

    *update_server.sh

--- script_actualizar/update_server.sh
+++ script_actualizar/update_server.sh
@@ -3,6 +3,11 @@
 # command=$( cat git_output_test.txt )
 command=$( git pull git@github.com:Macascript/companyday.git )
 
-if [[ ${command} == *"changed"* ]] || [[ ${command} == *"insertion"* ]] || [[ ${command} == *"insertion"* ]]; then
-    sudo /etc/init.d/apache2 restart
-fi
+while true
+do
+    if [[ ${command} == *"changed"* ]] || [[ ${command} == *"insertion"* ]] || [[ ${command} == *"insertion"* ]]; then
+        sudo /etc/init.d/apache2 restart
+    fi
+    sleep 1m
+    echo "Hecho"
+done
