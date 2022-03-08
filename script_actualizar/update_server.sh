cd6fc0c3a1b30db866f68049db69a7fd40047881
adrian <j.adrian.mazuecos@gmail.com>
2022-03-08 22:24:50 +0100

    *update_server.sh

--- script_actualizar/update_server.sh
+++ script_actualizar/update_server.sh
@@ -5,7 +5,7 @@
 while true
 do
     command=$( git pull https://github.com/Macascript/companyday.git )
-    if [[ ${command} == *"changed"* ]] || [[ ${command} == *"insertion"* ]] || [[ ${command} == *"insertion"* ]]; then
+    if [[ ${command} == *"app.py"* ]]; then
         sudo /etc/init.d/apache2 restart
     fi
     sleep 1m
